450a769cd66b3d68a4f544104456e129fc87bd68
constantine <fribbleart@gmail.com>
2022-10-22 20:15:00 +0800

    Created article scaffold

---
+++ db/migrate/20221022121103_create_articles.rb
@@ -0,0 +1,12 @@
+class CreateArticles < ActiveRecord::Migration[7.0]
+  def change
+    create_table :articles do |t|
+      t.references :user, null: false, foreign_key: true
+      t.string :title
+      t.text :body
+      t.datetime :published_date
+
+      t.timestamps
+    end
+  end
+end
