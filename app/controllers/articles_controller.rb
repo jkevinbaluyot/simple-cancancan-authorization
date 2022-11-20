509b24be50dc5c55d01ed9dd878cdf72ac4b28d2
constantine <fribbleart@gmail.com>
2022-11-20 17:43:51 +0800

    Added cancancan

--- app/controllers/articles_controller.rb
+++ app/controllers/articles_controller.rb
@@ -1,6 +1,6 @@
 class ArticlesController < ApplicationController
   before_action :authenticate_user!, except: %i[ show index ]
-  before_action :set_article, only: %i[ show edit update destroy ]
+  load_and_authorize_resource
 
   # GET /articles or /articles.json
   def index
@@ -54,11 +54,6 @@ class ArticlesController < ApplicationController
   end
 
   private
-    # Use callbacks to share common setup or constraints between actions.
-    def set_article
-      @article = Article.find(params[:id])
-    end
-
     # Only allow a list of trusted parameters through.
     def article_params
       params.require(:article).permit(:title, :body)
